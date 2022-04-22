92faf0d75d0f289bbd571dda56125a46f1912684
jorgealexis07 <201711104@tese.edu.mx>
2022-04-22 14:57:29 -0500

    paso 4. se modifico la prueba y se puseron las fechas en Undefined

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -3,7 +3,7 @@ const User = require('./../../app/models/user')
 describe("Unit Test for User class", () => {
     test('Create an User object', () => {
         //Aqui invocas el codigo que vas a usar en tu app
-        const user = new User(1, "jorgealexis", "Alexis", "Bio", "dateCreated", "lastUpdated")
+        const user = new User(1, "jorgealexis", "Alexis", "Bio")
 
         //Aqui validas los resultados de ese codigo.
         //Esta es una comparacion que va igualar el valor de la izquierda con el valor de la derecha (valor esperado)
@@ -11,7 +11,7 @@ describe("Unit Test for User class", () => {
         expect(user.username).toBe("jorgealexis")
         expect(user.name).toBe("Alexis")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     } )
 })
