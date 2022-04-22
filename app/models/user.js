143e622a07eea0ded69c6f7a4ac2d194adce6326
jorgealexis07 <201711104@tese.edu.mx>
2022-04-22 15:25:11 -0500

    paso 8. Se agrega el codigo para hacer que la prueba pase.

--- app/models/user.js
+++ app/models/user.js
@@ -24,6 +24,14 @@ class User {
         return this.lastUpdated
     }
 
+    set setUsername(newUsername){
+        this.username = newUsername
+    }
+
+    set setBio(newBio){
+        this.bio = newBio
+    }
+
 }
 
 module.exports = User
