1b7fa5fb3152d36932a6f5d16ab8f045fe92a041
jorgealexis07 <201711104@tese.edu.mx>
2022-04-22 19:43:25 -0500

    Paso 6: Pasa la prueba del requerimiento 3

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -8,6 +8,11 @@ class UserService{
     static getInfo(user){
         return Object.values(user)
     }
+
+    static updateUserUsername(user,username){
+        user.setUsername = username;
+    }
+
 }
 
 module.exports = UserService
