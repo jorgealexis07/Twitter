de3a825a0777dc95cf1401b51925a770b4d93a88
jorgealexis07 <201711104@tese.edu.mx>
2022-04-25 14:00:45 -0500

    Paso 3: Se realiza la prueba del requerimiento 3 del modulo de las vistas

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -9,10 +9,19 @@ test("Return an error object when try to create a new user with an null payload"
     expect(result.error).toMatch(/payload no existe/)
 } )
 
-test("Return an error object when try to create a new user with a payload with inavalid properties.", () =>{
+test("Return an error object when try to create a new user with a payload with inavalid properties", () =>{
     const payload = {username: null, name: 12, id: "id"}
     const result = UserView.createUser(payload)
-    expect(result.error).toMatch(/necesitan un valor valido/)
+    expect(result.error).toMatch(/necesitan tener un valor válido/)
 })
+
+
+test("Return an error object when try to create a new user with a payload with missing properties", () =>{
+    const payload = {username: "Username"}
+    const result = UserView.createUser(payload)
+    expect(result.error).toMatch(/necesitan tener un valor válido/)
+})
+
+
 
 })
