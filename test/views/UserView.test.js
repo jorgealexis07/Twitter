86606a5f41306ef6f477c2ddaab952acaddb9411
jorgealexis07 <201711104@tese.edu.mx>
2022-04-25 12:43:40 -0500

    Paso 2: Se realiza la prueba del requerimiento 2 del modulo de las vistas

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -9,4 +9,10 @@ test("Return an error object when try to create a new user with an null payload"
     expect(result.error).toMatch(/payload no existe/)
 } )
 
+test("Return an error object when try to create a new user with a payload with inavalid properties.", () =>{
+    const payload = {username: null, name: 12, id: "id"}
+    const result = UserView.createUser(payload)
+    expect(result.error).toMatch(/necesitan un valor valido/)
+})
+
 })
