91e7a84dcc8966c791c153e6a3577d0c4164abed
jorgealexis07 <201711104@tese.edu.mx>
2022-04-25 14:09:50 -0500

    Paso 4: Se realiza la prueba del requerimiento 4 del modulo de las vistas

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -22,6 +22,12 @@ test("Return an error object when try to create a new user with a payload with m
     expect(result.error).toMatch(/necesitan tener un valor válido/)
 })
 
-
+test("Create a user by a given valid payload", () =>{
+    const payload = {username: "username", id: 1, name: "name"}
+    const result = UserView.createUser(payload)
+    expect(result.name).toBe("name")
+    expect(result.username).toBe("username")
+    expect(result.id).toBe(1)
+})
 
 })
